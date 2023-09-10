6d4973ee12d218dc19d9014a1d15fc66362358c0
Hannah Kuklinski <hmkuklinski@gmail.com>
2023-09-10 12:34:55 -0400

    added calculator_practice.py to codedex repo
    practicing defining functions and calling methods

---
+++ calculator_practice.py
@@ -0,0 +1,29 @@
+#calculator- practicing defining functions
+
+#addition function
+def add(a,b):
+  return a+b
+  
+#subtraction function
+def subtract(a,b):
+  return a-b
+
+#multiply function
+def multiply(a,b):
+  return a*b
+
+#divide function
+def divide(a,b):
+  return a/b
+
+#exponent function
+def exp(a,b):
+  return a**b
+
+#calling the function and printing the return value
+
+print(add(5,5))
+print(subtract(5,5))
+print(multiply(5,5))
+print(divide(5,5))
+print(exp(5,2))
